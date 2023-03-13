e36d7ff7c696cc8fc5802341b11ec5f3247e68d6
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-03-13 15:57:00 +0700

    Basic layout

--- app/src/main/java/live/ryyvv/fumoku/MainActivity.kt
+++ app/src/main/java/live/ryyvv/fumoku/MainActivity.kt
@@ -1,11 +1,34 @@
 package live.ryyvv.fumoku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val topAppBar = findViewById<Toolbar>(R.id.topAppBar)
+        setSupportActionBar(topAppBar)
+
+        val bottomAppBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
+        bottomAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.about -> {
+                    startActivity(
+                        Intent(
+                            this,
+                            AboutActivity::class.java
+                        )
+                    )
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
